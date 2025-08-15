54d0e0227f2067cf26a1d452ecd8588bf400bf5f
Lipe manza <luizffrances@gmail.com>
2025-08-15 18:49:06 -0300

    just ajusting somethings

---
+++ first_project/src/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int resultFloat = (int)Math.ceil(1.1F);
+        int resultInteger = Math.round(1.1F);
+        System.out.println(resultFloat);
+            System.out.println(resultInteger);
+        }
+    }
